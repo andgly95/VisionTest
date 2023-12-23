4a32b17d5f2d438cefba24532314b1e7241ba57a
Andrew Glyadchenko <andrewgly95@gmail.com>
2023-12-23 17:32:51 -0500

    Initial Commit

---
+++ VisionTest/VisionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionTestApp.swift
+//  VisionTest
+//
+//  Created by Andrew Glyadchenko on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct VisionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
